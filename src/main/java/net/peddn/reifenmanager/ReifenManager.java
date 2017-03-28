403c35376cf2c43f46d27796b6b1c7e07242e65a
Thomas Schmid <areadox@gmail.com>
2017-03-28 19:02:19 +0200

    updated broadcast method

---
+++ src/main/java/net/peddn/reifenmanager/ReifenManager.java
@@ -0,0 +1,9 @@
+package net.peddn.reifenmanager;
+
+public class ReifenManager {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
