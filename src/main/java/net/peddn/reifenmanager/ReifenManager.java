aa54ce916cbc63cbf43868649fddc7756cf11a8c
peddn <peddn@DESKTOP-G8C3ARM.fritz.box>
2017-03-28 19:35:03 +0200

    heavy developement

--- src/main/java/net/peddn/reifenmanager/ReifenManager.java
+++ src/main/java/net/peddn/reifenmanager/ReifenManager.java
@@ -1,9 +1,32 @@
 package net.peddn.reifenmanager;
 
+import com.mongodb.MongoClient;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+
 public class ReifenManager {
-    /**
-     * @param args the command line arguments
-     */
+
+    private Morphia morphia = new Morphia();
+
+    public Datastore datastore;
+    
     public static void main(String[] args) {
+        ReifenManager reifenManager = new ReifenManager();
+        reifenManager.createDatastore();
     }
+
+    public ReifenManager() {
+
+        // tell Morphia where to find your classes
+        // can be called multiple times with different packages or classes
+        //ReifenManager.MORPHIA.mapPackage("org.mongodb.morphia.example");
+
+    }
+    
+    private void createDatastore() {
+        // create the Datastore connecting to the default port on the local host
+        this.datastore = this.morphia.createDatastore(new MongoClient(), "reifenmanager");
+        datastore.ensureIndexes();
+    }
+
 }
